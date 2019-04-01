7cfa0d220668fb1937cc6edbfbddb2e0f78f7a45
wilkinsb <brandwilki@gmail.com>
2019-04-01 15:10:31 -0700

    write some tests that intentionally fail

---
+++ tests/unit/test_task_fail.py
@@ -0,0 +1,16 @@
+"""Use the 'Task' datatype to show test failures"""
+from src.tasks import Task
+
+
+def test_task_equality():
+    """different Tasks should not be equal"""
+    t1 = Task('sit there', 'brandon')
+    t2 = Task('do something', 'wilkins')
+    assert t1 == t2
+
+
+def test_dict_equality():
+    """Different Tasks compared as dicts should not be equal"""
+    t1_dict = Task('play ball', 'brandon')._asdict()
+    t2_dict = Task('make lunch', 'wilkins')._asdict()
+    assert t1_dict == t2_dict
