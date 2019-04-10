dcbe6ceb7365fb9909e6d0d3d419d3daf662b66c
wilkinsb <brandwilki@gmail.com>
2019-04-10 11:39:54 -0700

    skip example failing tests for now

--- tests/unit/test_task_fail.py
+++ tests/unit/test_task_fail.py
@@ -1,14 +1,17 @@
 """Use the 'Task' datatype to show test failures"""
+import pytest
 from src.tasks import Task
 
 
+@pytest.mark.skip(reason='Expected to fail')
 def test_task_equality():
     """different Tasks should not be equal"""
-    t1 = Task('sit there', 'brandon')
-    t2 = Task('do something', 'wilkins')
-    assert t1 == t2
+    task_1 = Task('sit there', 'brandon')
+    task_2 = Task('do something', 'wilkins')
+    assert task_1 == task_2
 
 
+@pytest.mark.skip(reason='Expected to fail')
 def test_dict_equality():
     """Different Tasks compared as dicts should not be equal"""
     t1_dict = Task('play ball', 'brandon')._asdict()
